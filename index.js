8b19e75a40415a265f7ce6de951752ce899d2d4f
prksgroup <52328431+prksgroup@users.noreply.github.com>
2019-08-31 10:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const port = 8000;
-const cookieParser = require('cookie-parser');
 const app = express();
+const cookieparser = require('cookie-parser');
 const expresslayout = require('express-ejs-layouts');
-const userschema = require('./models/user');
+const userschema = require('./models/socialdb');
 const db = require('./config/mongoose');
-
 const session = require('express-session');
 const passport = require('passport');
-const passportlocal = require('./config/passport');
+const passportlocal = require('./config/passport-local-strategy');
+const mongo = require('connect-mongo')(session);
+const sassmiddleware = require('node-sass-middleware');
+
+app.use(sassmiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
 
 app.use(express.urlencoded());
 
-app.use(cookieParser());
-
+app.use(cookieparser());
 app.use(express.static('./assets'));
 app.use(expresslayout);
 
@@ -26,19 +34,25 @@ app.set('layout extractScript', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
 app.use(session({
     name: 'SOCIAL',
-    secret: 'parthsharma',
+    secret: 'PARTH',
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 1000)
-    }
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new mongo({
+        mongooseConnection: db,
+        autoremove: 'disabled'
+    }, function(err) { console.log(err || 'OKAY WORKING SUCCESSFULLY') })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
 
 app.use('/', require('./routes/index'));
 
@@ -48,3 +62,42 @@ app.listen(port, function(err) {
     }
     console.log(`Server is running successfully at :${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const session = require('express-session');
+// const passport = require('passport');
+// const passportlocal = require('./config/passport');
+// const sassmiddleware = require('node-sass-middleware');
+
+
+
+
+
+
+// app.use(session({
+//     name: 'SOCIAL',
+//     secret: 'parthsharma',
+//     saveUninitialized: false,
+//     resave: false,
+//     cookie: {
+//         maxAge: (1000 * 60 * 1000)
+//     }
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
