1c70eeb0d1eb7d2a34471960cf9891c459fd21f7
prksgroup <52328431+prksgroup@users.noreply.github.com>
2019-09-08 00:28:30 +0530

    Update post_controller.js

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 const postuser = require('../models/post');
-
+const Comment = require('../models/comments');
 
 module.exports.create = function(req, res) {
     postuser.create({
@@ -10,5 +10,24 @@ module.exports.create = function(req, res) {
             console.log('errrrrr');
         }
         return res.redirect('back');
+    })
+}
+
+module.exports.destroy = function(req, res) {
+    postuser.findById(req.params.id, function(err, post) {
+        console.log(post.user);
+        console.log(req.user.id);
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({ post: req.params.id }, function(err) {
+                if (err) { console.log('ERROR IN DELETING POST AND COMMENTS') } else {
+                    return res.redirect('back');
+                }
+            })
+
+        } else {
+            return res.redirect('back');
+        }
     })
 }
